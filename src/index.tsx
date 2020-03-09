f69f466bca874e2415154a335b9d3b4f25d2dfcb
André Markwalder <andre.markwalder@gmail.com>
2020-03-09 04:28:43 +0100

    Fixed title and localhost auth handling

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Amplify from 'aws-amplify';
 import config from './aws-exports';
 
+const isLocalhost = Boolean(
+    window.location.hostname === 'localhost' ||
+    window.location.hostname === '[::1]' ||
+    window.location.hostname.match(
+        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+);
+
+if (isLocalhost) {
+    config.oauth.redirectSignIn = 'http://localhost:3000/';
+    config.oauth.redirectSignOut = 'http://localhost:3000/';
+}
+
+
 Amplify.configure(config);
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href') || "/";
